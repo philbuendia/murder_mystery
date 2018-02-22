35ef6b1c9b330b297ff355f10d360eed3b2e8ff9
Philip Buendia <35639267+philbuendia@users.noreply.github.com>
2018-02-22 12:29:45 -0800

    Phil

--- murder_mystery.js
+++ murder_mystery.js
@@ -1,25 +1,19 @@
-var room = "dining room";
-var suspect = "Mr. Parkes";
+var room = "ballroom";
+var suspect = "Mr. Kalehoff";
 
 var weapon = "";
 var solved = false;
 
-if (room ==="ballroom" && suspect === "Mr.Kalehoff") {
-   weapon = "poison";
-   solved = true;
-   
-} else if (room === "gallery" && suspect === "Ms. Van Cleve") {
-   weapon = "trophy";
-   solved = false;
-   
-} else if (room === "billiards" && suspect === "Mrs. Sparr") {
-   weapon = "pool stick";
-   solved = true;
-   
-} else if (room === "dining room" && suspect === "Mr. Parkes") {
-   weapon = "knife";
-   solved = true;
+if (room === "ballroom") { weapon = "poison"; solved = true;
+    
+} else if (room === "gallery") { weapon = "trophy"; solved = true;
+    
+} else if (room === "billiards room") { weapon = "pool stick"; solved = true;
+    
+} else { weapon = "knife"; solved = true;
+    
 }
+
 if (solved) {
-    console.log(suspect + " did it in the " + room + " with the " + weapon + "!");
+	console.log(suspect + " did it in the " + room + " with the " + weapon + "!");
 }
