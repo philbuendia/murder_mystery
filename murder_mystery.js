bacd80fc0cd913be0585b95f980d151418e06916
Philip Buendia <35639267+philbuendia@users.noreply.github.com>
2018-02-22 21:10:40 -0800

    Revert "Phil"
    This reverts commit 35ef6b1c9b330b297ff355f10d360eed3b2e8ff9.

--- murder_mystery.js
+++ murder_mystery.js
@@ -1,10 +1,13 @@
-var room = "ballroom";
-var suspect = "Mr. Kalehoff";
+var room = "dining room";
+var suspect = "Mr. Parkes";
 
 var weapon = "";
 var solved = false;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 35ef6b1... Phil
 if (room ==="ballroom" && suspect === "Mr.Kalehoff") {
    weapon = "poison";
    solved = true;
@@ -12,7 +15,11 @@ if (room ==="ballroom" && suspect === "Mr.Kalehoff") {
 } else if (room === "gallery" && suspect === "Ms. Van Cleve") {
    weapon = "trophy";
 <<<<<<< HEAD
+<<<<<<< HEAD
    solved = true;
+=======
+   solved = false;
+>>>>>>> parent of 35ef6b1... Phil
 =======
    solved = false;
 >>>>>>> parent of 35ef6b1... Phil
@@ -24,6 +31,7 @@ if (room ==="ballroom" && suspect === "Mr.Kalehoff") {
 } else if (room === "dining room" && suspect === "Mr. Parkes") {
    weapon = "knife";
    solved = true;
+<<<<<<< HEAD
 =======
 if (room === "ballroom") { weapon = "poison"; solved = true;
     
@@ -34,8 +42,9 @@ if (room === "ballroom") { weapon = "poison"; solved = true;
 } else { weapon = "knife"; solved = true;
     
 >>>>>>> parent of 3ede75d... Revert "Phil"
+=======
+>>>>>>> parent of 35ef6b1... Phil
 }
-
 if (solved) {
-	console.log(suspect + " did it in the " + room + " with the " + weapon + "!");
+    console.log(suspect + " did it in the " + room + " with the " + weapon + "!");
 }
