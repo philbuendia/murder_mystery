f8aab4a494c6eb8073efa91894af7a26a5c399c7
Philip Buendia <35639267+philbuendia@users.noreply.github.com>
2018-02-22 21:10:15 -0800

    Revert "Revert "Phil""
    This reverts commit 3ede75d913be17b3b6c7fb0a6af0773334865c9f.

--- murder_mystery.js
+++ murder_mystery.js
@@ -1,9 +1,10 @@
-var room = "dining room";
-var suspect = "Mr. Parkes";
+var room = "ballroom";
+var suspect = "Mr. Kalehoff";
 
 var weapon = "";
 var solved = false;
 
+<<<<<<< HEAD
 if (room ==="ballroom" && suspect === "Mr.Kalehoff") {
    weapon = "poison";
    solved = true;
@@ -23,7 +24,18 @@ if (room ==="ballroom" && suspect === "Mr.Kalehoff") {
 } else if (room === "dining room" && suspect === "Mr. Parkes") {
    weapon = "knife";
    solved = true;
+=======
+if (room === "ballroom") { weapon = "poison"; solved = true;
+    
+} else if (room === "gallery") { weapon = "trophy"; solved = true;
+    
+} else if (room === "billiards room") { weapon = "pool stick"; solved = true;
+    
+} else { weapon = "knife"; solved = true;
+    
+>>>>>>> parent of 3ede75d... Revert "Phil"
 }
+
 if (solved) {
-    console.log(suspect + " did it in the " + room + " with the " + weapon + "!");
+	console.log(suspect + " did it in the " + room + " with the " + weapon + "!");
 }
